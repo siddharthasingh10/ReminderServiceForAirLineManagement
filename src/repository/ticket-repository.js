107e4968946dc3e306210e6bb3194e4ab1367039
Siddhartha singh <rajputsiddhartha2222@gmail.com>
2024-10-09 23:09:59 +0530

    done with sending mails

--- src/repository/ticket-repository.js
+++ src/repository/ticket-repository.js
@@ -33,6 +33,19 @@ class TicketRepository{
         } catch (error) {
             throw error;
         }
-    }  
+    } 
+    async update(ticketId, data) {
+        try {
+            const ticket = await NotificationTicket.findByPk(ticketId);
+            if(data.status)
+                ticket.status = data.status;
+            await ticket.save();
+            return ticket;
+        } catch (error) {
+            throw error;
+        }
+    }
 }
+ 
+
 module.exports=TicketRepository;
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,15 +1,15 @@
-const transporter = require('./../config/emailConfig');
+const sender = require('./../config/emailConfig');
 const TicketRepository = require('./../repository/ticket-repository');
 const repo = new TicketRepository();
 
 const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
     try {
-        const info = await transporter.sendMail({
+        const info = await sender.sendMail({
             from: mailFrom,
             to: mailTo,
             subject: mailSubject,
             text: mailBody
-        });
+        }); 
         console.log(`Email sent to ${mailTo}: ${info.response}`);
     } catch (error) {
         console.error(`Failed to send email to ${mailTo}:`, error);
@@ -33,5 +33,13 @@ const createNotification = async (data) => {
         console.error('Error creating notification:', error);
     }
 };
+const updateTicket = async (ticketId, data) => {
+    try {
+        const response = await repo.update(ticketId, data);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-module.exports = { sendBasicEmail, fetchPendingEmails, createNotification };
+module.exports = { sendBasicEmail, fetchPendingEmails, createNotification,updateTicket };
